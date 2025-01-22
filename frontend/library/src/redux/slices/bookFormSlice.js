cb98fb77bd6968a31bb04fbeffb56d367137ae41
onenariman <narikanm@gmail.com>
2025-01-22 23:33:06 +0300

    Добавили другую функцию для extraReducers

--- frontend/library/src/redux/slices/bookFormSlice.js
+++ frontend/library/src/redux/slices/bookFormSlice.js
@@ -39,6 +39,16 @@ const bookFormSlice = createSlice({
         },
     },
 
+    //Option 1
+
+    // extraReducers: {
+    //     [fetchBook.fulfilled]: (state, action) => {
+    //         if (action.payload.title && action.payload.author) {
+    //             state.push(createBookWithID(action.payload, "API"));
+    //         }
+    //     },
+    // },
+
     extraReducers: (builder) => {
         builder.addCase(fetchBook.fulfilled, (state, action) => {
             if (action.payload.title && action.payload.author) {

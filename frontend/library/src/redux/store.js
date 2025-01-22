324cdb79319c26235f0fb5bc5c106ff95dc4b111
onenariman <narikanm@gmail.com>
2025-01-22 22:37:22 +0300

    Создали errorSlice

--- frontend/library/src/redux/store.js
+++ frontend/library/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterSlice from "./slices/filterSlice";
 import bookFormSlice from "./slices/bookFormSlice";
+import errorSlice from "./slices/errorSlice";
 
 const store = configureStore({
     reducer: {
         filter: filterSlice,
         books: bookFormSlice,
+        error: errorSlice,
     },
 });
 
